c2bc274d7a8b6ac4925738c48d3a279e53a79fb9
BhanutejGaddam <Bhanutej.Gaddam@cognizant.com>
2026-06-27 16:45:01 +0530

    Add files via upload

---
+++ SQLWalkRepository.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class SQLWalkRepository : IWalkRepository
+    {
+        private readonly NZWalksDbContext dbContext;
+        public SQLWalkRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Walk> CreateAsync(Walk walk)
+        {
+            await dbContext.Walks.AddAsync(walk);
+            await dbContext.SaveChangesAsync();
+            return walk;
+        }
+
+        public async Task<Walk> DeleteAsync(Guid id)
+        {
+            var walk = await dbContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+            if (walk == null)
+            {
+                return null;
+            }
+
+            dbContext.Walks.Remove(walk);
+            await dbContext.SaveChangesAsync();
+            return walk;
+        }
+
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null, string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000)
+        {
+            //Doesn't return region the region and difficulty of the walk
+            //return await dbContext.Walks.ToListAsync();
+            //This returns the region and difficulty of the walk
+            //return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
+            var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable(); //Use lambda expressions here
+            //Filtering
+            if(string.IsNullOrWhiteSpace(filterOn)==false && string.IsNullOrWhiteSpace(filterQuery) == false)
+            {
+                if (filterOn.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = walks.Where(x => x.Name.Contains(filterQuery));
+                }
+            }
+
+            //Sorting
+            if (string.IsNullOrWhiteSpace(sortBy) == false)
+            {
+                if (sortBy.Equals("Name",StringComparison.OrdinalIgnoreCase)) {
+                    walks = isAscending ? walks.OrderBy(x => x.Name) : walks.OrderByDescending(x => x.Name);
+                }
+                else if(sortBy.Equals("Length", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.LengthInKm) : walks.OrderByDescending(x => x.LengthInKm);
+                }
+            }
+
+            //Pagination
+            var skipResults = (pageNumber) - 1 * pageSize;
+
+            return await walks.Skip(skipResults).Take(pageSize).ToListAsync();
+        }
+
+        public async Task<Walk> GetWalkByIdAsync(Guid id)
+        {
+            return await dbContext.Walks.Include("Difficulty").Include("Region").FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Walk> UpdateWalkAsync(Guid id, Walk walk)
+        {
+            var existingWalk = await dbContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingWalk==null)
+            {
+                return null;
+            }
+            existingWalk.Name = walk.Name;
+            existingWalk.Description=walk.Description;
+            existingWalk.LengthInKm = walk.LengthInKm;
+            existingWalk.DifficultyId = walk.DifficultyId;
+            existingWalk.RegionId = walk.RegionId;
+
+            await dbContext.SaveChangesAsync();
+
+            return existingWalk;
+        }
+    }
+}
